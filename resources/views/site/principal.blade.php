6004f7d401f1128a751aeb87541f29e97b43a840
Maike Naysinger Borges <mbnaysinger@MacBook-Air-de-Maike.local>
2023-09-24 20:57:14 -0300

    advanced routes

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,13 @@
 <h3>teste de view</h3>
+
+<ul>
+    <li>
+        <a href="{{ route('site.index') }}">Pincipal</a>
+    </li>
+    <li>
+        <a href="{{ route('site.sobrenos') }}">Sobre Nós</a>
+    </li>
+    <li>
+        <a href="{{ route('laravel.index') }}">Laravel</a>
+    </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/", [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get("/", [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get("/contato", [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get("/sobrenos", [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; })->name('site.clientes');
+    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('site.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('site.produtos');
+});
+
+Route::get('/rota1', function() { echo 'Rota 1'; })->name('site.rota1');
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
 
 Route::get("/teste/{nome}/{idade}", function(string $nome, string $idade) {
     echo 'Estamos aqui: '.$nome.' - Idade: '.$idade.' Anos';
 });
 
-Route::get('/teste', function () {
+Route::get('/laravel', function () {
     return view('welcome');
+})->name('laravel.index');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe.
+    <a href="'.route('site.index').'">Clique aqui</a> para acessar a página incial.';
 });
